d67480ce9ec79e44d0cf4df73d40b9e88803a233
RidmiR <ridmi@wso2.com>
2019-02-20 10:29:29 +0530

    Add aggregate report genarate source code

---
+++ product-scenarios/aggregate/src/test/get-results.py
@@ -0,0 +1,37 @@
+import shutil
+import os
+
+dest='${HOME}/aggregate/test-result'
+start = '${HOME}'
+
+# read all the TEST-TestSuite.xml and write the file paths to the new text file
+#  to get the count of the testsuit.xml files
+filepath = open('${HOME}/aggregate/path.txt', 'w')
+for dirpath, dirnames, filenames in os.walk(start):
+    for filename in filenames:
+        if filename == "TEST-TestSuite.xml":
+            filename = os.path.join(dirpath, filename)
+            print(filename)
+            filepath.write('\n' + filename)
+filepath.close()
+
+# get the TEST-TestSuite.xml file count
+count = len(open('${HOME}/aggregate/path.txt').readlines(  ))
+
+# rename the TEST-TestSuite.xml files by adding a index as a suffix and
+# copying TEST-TestSuite.xml file to test-result folder inside aggregate
+ii = 1
+while ii < count :
+    for dirpath, dirnames, filenames in os.walk(start):
+        for filename in filenames:
+            if filename == "TEST-TestSuite.xml":
+                old_name = os.path.join(dirpath, filename )
+                dir_path = os.path.join(dirpath)
+                base, extension = os.path.splitext(filename)
+                newname = os.path.join(dirpath, base + "_" + str(ii) + extension)
+                os.chdir(dir_path)
+                os.rename(old_name,newname)
+                shutil.copy(newname, dest)
+                ii = ii + 1
+                break
+quit()
